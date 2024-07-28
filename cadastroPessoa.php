750b0473c80c89b1340a12694ff22ae0eb519021
marikm <matsumotomarina3@gmail.com>
2024-07-28 15:00:27 -0300

    variaveis do formulario

---
+++ cadastroPessoa.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+  <html>
+    <head>
+      <!--Import Google Icon Font-->
+      <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+      <!--Import materialize.css-->
+      <link type="text/css" rel="stylesheet" href="css/materialize.min.css"  media="screen,projection"/>
+
+      <!--Let browser know website is optimized for mobile-->
+      <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
+    </head>
+
+    <body>
+        <div class="container #e1f5fe light-blue lighten-5 center">
+            <?php
+                $nomePessoa = $_POST['nomePessoa'];
+                $endereco = $_POST['endereco'];
+                $telefone = $_POST['telefone'];
+                $email = $_POST['email'];
+                $dataNascimento = $_POST['dataNascimento'];
+            ?>
+            
+
+        </div>
+        
+
+
+      <!--JavaScript at end of body for optimized loading-->
+      <script type="text/javascript" src="js/materialize.min.js"></script>
+    </body>
+  </html>
